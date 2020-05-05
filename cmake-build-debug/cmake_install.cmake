383f71fcf77e8f3221f97eb931f3793b24915b59
Aldo Cambronero Ureña <cambroneroaldo03@gmail.com>
2020-05-05 17:49:28 -0600

    Cambios del smart pointee

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/shakime/Desktop/CodeWorks/Design-patterns
+# Install script for directory: /home/aldo/CLionProjects/Datos 2 - 2.0
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/shakime/Desktop/CodeWorks/Design-patterns/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/aldo/CLionProjects/Datos 2 - 2.0/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
